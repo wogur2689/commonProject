320cfc97d0855c1c6828c4dfb69a13ee387c1d45
길재혁 <81075170+wogur2689@users.noreply.github.com>
2023-10-19 16:52:03 +0900

    [feat] 결제 백엔드 로직 시작

--- src/main/java/com/example/commonproject/common/error/CommonErrorController.java
+++ src/main/java/com/example/commonproject/common/error/CommonErrorController.java
@@ -5,10 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
