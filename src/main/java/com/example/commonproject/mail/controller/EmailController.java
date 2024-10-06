920203788591bd2f7f220c2f18d86538d6a5711e
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-10-06 21:02:01 +0900

    feat : EmailController

--- src/main/java/com/example/commonproject/mail/controller/EmailController.java
+++ src/main/java/com/example/commonproject/mail/controller/EmailController.java
@@ -1,52 +1,24 @@
 package com.example.commonproject.mail.controller;
 
-import com.example.commonproject.mail.dto.EmailDto;
-import com.example.commonproject.mail.service.EmailService;
-import com.example.commonproject.mail.util.EmailEnum;
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@RestController
+@Controller
+@RequestMapping("/mail")
 public class EmailController {
+    @GetMapping
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("mail");
+        return mav;
+    }
 
-    @Resource
-    private EmailService emailService;
-
-    /**
-     * 메일 보내기
-     */
-    @PostMapping("/mailSend")
-    public ModelAndView mailSendPost(ModelAndView mav, HttpServletRequest request, BindingResult result) {
-        String code = "0000";
-
-        //1. 요청값 검증
-        if(result.hasErrors()) {
-            log.error("요청값 에러 {}", result.getTarget());
-            code = "0001";
-        }
-
-        EmailDto emailDto = EmailDto.builder()
-                .toUser(request.getParameter("toUser"))
-                .ccUser(request.getParameter("ccUser"))
-                .bccUser(request.getParameter("bccUser"))
-                .fromAddress(request.getParameter("fromAddress"))
-                .subject(request.getParameter("subject"))
-                .content(request.getParameter("content"))
-                .build();
-
-        //2. 메일 전송
-        code = emailService.sendMailSimple(emailDto);
-
-        //3, 결과 반환
-        mav.addObject("code", code);
-        mav.addObject("msg", EmailEnum.getMessage(code));
-        mav.setViewName("callBack");
+    @GetMapping("/mailResult")
+    public ModelAndView callBack(ModelAndView mav) {
+        mav.setViewName("mailResult");
         return mav;
     }
 }
